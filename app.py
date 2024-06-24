f71368c29328d1bffb2acc6784eff2817b2d7f84
kavyasri2099 <149765449+kavyasri2099@users.noreply.github.com>
2024-06-24 17:30:44 +0530

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 from PIL import Image, ImageOps
 import pickle
+import os
 
 # Load the model and preprocessing pipeline
 with open('rf_model.pkl', 'rb') as model_file:
@@ -12,34 +13,25 @@ with open('preprocessing_pipeline.pkl', 'rb') as pipeline_file:
 
 categories = ["cloudy", "desert", "green_area", "water"]
 
-def augment_image(image):
-    angle = np.random.uniform(-20, 20)
-    image = image.rotate(angle)
-    if np.random.rand() > 0.5:
-        image = ImageOps.mirror(image)
-    return image
-
 def preprocess_image(image):
     image = image.resize((128, 128))
-    image = augment_image(image)
     image = ImageOps.grayscale(image)
-    image_array = np.array(image).flatten().reshape(1, -1)
-    image_array = preprocessing_pipeline.transform(image_array)
-    return image_array
+    img_array = np.array(image).flatten().reshape(1, -1)
+    img_array = preprocessing_pipeline.transform(img_array)
+    return img_array
 
 st.title("Satellite Image Classification App")
-st.write("Upload an image to classify it into one of the following categories: cloudy, desert, green_area, water.")
+st.write("Upload an image and the model will classify it into one of the following categories: Cloudy, Desert, Green Area, Water")
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Classifying...")
     
-    image_array = preprocess_image(image)
-    prediction = rf_model.predict(image_array)
+    img_array = preprocess_image(image)
+    prediction = rf_model.predict(img_array)
     category = categories[prediction[0]]
-    
     st.write(f"The image is classified as: {category}")
